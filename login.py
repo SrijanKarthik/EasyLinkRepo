77b54efc6a0bdbaaa513dfe7511cebbcc0d41a92
SrijanKarthik <85946826+SrijanKarthik@users.noreply.github.com>
2021-06-22 19:13:01 +0200

    Update login.py

--- login.py
+++ login.py
@@ -1,3 +1,6 @@
+##this is by no means a finished product and doesn\t even include formatting or text for the actual admin login page. this is simply a demo for a program that uses sql as a database for a login system
+
+##importing tkinter which is a python addon that helps with this type of program
 from tkinter import *
 import tkinter.messagebox
 import mysql.connector
@@ -7,29 +10,12 @@ import mysql.connector
 connectiondb = mysql.connector.connect(host="localhost",user="root",passwd="",database="logindb")
 cursordb = connectiondb.cursor()
 
-
+## the following commands are for things that can occur when running the program, their outputs have not been configures yet
 def login():
     global root2
-    root2 = Toplevel(root)
-    root2.title("Account Login")
-    root2.geometry("450x300")
-    root2.config(bg="white")
-
     global username_verification
     global password_verification
-    Label(root2, text='Please Enter your Account Details', bd=5,font=('arial', 12, 'bold'), relief="groove", fg="white",
-                   bg="blue",width=300).pack()
-    username_verification = StringVar()
-    password_verification = StringVar()
-    Label(root2, text="").pack()
-    Label(root2, text="Username :", fg="black", font=('arial', 12, 'bold')).pack()
-    Entry(root2, textvariable=username_verification).pack()
-    Label(root2, text="").pack()
-    Label(root2, text="Password :", fg="black", font=('arial', 12, 'bold')).pack()
-    Entry(root2, textvariable=password_verification, show="*").pack()
-    Label(root2, text="").pack()
-    Button(root2, text="Login", bg="blue", fg='white', relief="groove", font=('arial', 12, 'bold'),command=login_verification).pack()
-    Label(root2, text="")
+ 
 
 def logged_destroy():
     logged_message.destroy()
@@ -41,22 +27,11 @@ def failed_destroy():
 def logged():
     global logged_message
     logged_message = Toplevel(root2)
-    logged_message.title("Welcome")
-    logged_message.geometry("500x100")
-    Label(logged_message, text="Login Successfully!... Welcome {} ".format(username_verification.get()), fg="green", font="bold").pack()
-    Label(logged_message, text="").pack()
-    Button(logged_message, text="Logout", bg="blue", fg='white', relief="groove", font=('arial', 12, 'bold'), command=logged_destroy).pack()
-
-
+   
 def failed():
     global failed_message
     failed_message = Toplevel(root2)
-    failed_message.title("Invalid Message")
-    failed_message.geometry("500x100")
-    Label(failed_message, text="Invalid Username or Password", fg="red", font="bold").pack()
-    Label(failed_message, text="").pack()
-    Button(failed_message,text="Ok", bg="blue", fg='white', relief="groove", font=('arial', 12, 'bold'), command=failed_destroy).pack()
-
+    
 
 def login_verification():
     user_verification = username_verification.get()
@@ -70,28 +45,6 @@ def login_verification():
             break
     else:
         failed()
-
-def Exit():
-    wayOut = tkinter.messagebox.askyesno("Login System", "Do you want to exit the system")
-    if wayOut > 0:
-        root.destroy()
-        return
-
-def main_display():
-    global root
-    root = Tk()
-    root.config(bg="white")
-    root.title("Login System")
-    root.geometry("500x500")
-    Label(root,text='Welcome to Log In System',  bd=20, font=('arial', 20, 'bold'), relief="groove", fg="white",
-                   bg="blue",width=300).pack()
-    Label(root,text="").pack()
-    Button(root,text='Log In', height="1",width="20", bd=8, font=('arial', 12, 'bold'), relief="groove", fg="white",
-                   bg="blue",command=login).pack()
-    Label(root,text="").pack()
-    Button(root,text='Exit', height="1",width="20", bd=8, font=('arial', 12, 'bold'), relief="groove", fg="white",
-                   bg="blue",command=Exit).pack()
-    Label(root,text="").pack()
 
 main_display()
 root.mainloop()
